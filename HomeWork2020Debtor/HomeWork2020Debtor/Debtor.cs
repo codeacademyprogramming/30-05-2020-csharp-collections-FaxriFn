8e0086339542234789dc82c3002750281e588bac
FaxriFn <fakhrifn@code.edu.az>
2020-06-05 19:57:31 +0400

    Collections

---
+++ HomeWork2020Debtor/HomeWork2020Debtor/Debtor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2020Debtor
+{
+    class Debtor
+    {
+        public Debtor(string fullname, DateTime birthDay, string phone, string email, string address, int debt)
+        {
+            (FullName, BirthDay, Phone, Email, Address, Debt) = (fullname, birthDay, phone, email, address, debt);
+        }
+
+        public string FullName { get; set; }
+        public DateTime BirthDay { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public int Debt { get; set; }
+        public override string ToString()
+        {
+            return $"{FullName} {BirthDay.ToShortDateString()} {Phone} {Email} {Address} {Debt}";
+        }
+    }
+}
